ee6c2e1a17dc1ec298df43f33c82ad9b2087a075
Caleb <calebsaldanhawork@gmail.com>
2024-09-06 15:59:11 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,50 @@
+let livros = [
+    {
+        titulo: "Memórias Póstumas de Brás Cubas",
+        resumo: "Considerado um marco da literatura brasileira, este romance apresenta um narrador irônico e crítico, que reflete sobre a vida e a morte de forma singular",
+        imagem: "https://static.mundoeducacao.uol.com.br/mundoeducacao/conteudo_legenda/4230f70d3f6efaefb1b0f82d0d705f9e.jpg",
+        tags: "memorias memórias memórias póstumas memorias postumas Memórias Póstumas bras cubas Brás Cubas"
+    },
+    {
+        titulo: "Dom Casmurro",
+        resumo: "Outro clássico de Machado de Assis, este romance psicológico explora temas como ciúme, traição e a construção da identidade",
+        imagem: "https://m.media-amazon.com/images/I/51BxlPc6-sL._SY580_.jpg",
+        tags: "dom casmurro Dom Casmurro"
+    },
+    {
+        titulo: "Iracema",
+        resumo: "Romance indianista que idealiza a figura do índio e a natureza brasileira, contribuindo para a formação da identidade nacional",
+        imagem: "https://armazemdacultura.com.br/cdn/shop/products/CapaHipnoseTanatosemIracemadeJosedeAlencar-1_grande.jpg?v=1596546630",
+        tags: "iracema Iracema"
+    },
+    {
+        titulo: "Quincas Borba",
+        resumo: "Neste romance, Machado de Assis satiriza a sociedade brasileira e a busca por felicidade através do dinheiro",
+        imagem: "https://m.media-amazon.com/images/I/61k9Y9fwZ6L._AC_UF1000,1000_QL80_.jpg",
+        tags: "quincas borba Quincas Borba"
+    },
+    {
+        titulo: "Vidas Secas",
+        resumo: "Um retrato realista e comovente da vida de uma família de retirantes no sertão nordestino",
+        imagem: "https://m.media-amazon.com/images/I/618-b9Im6dL._AC_UF1000,1000_QL80_.jpg",
+        tags: "vidas secas Vidas Secas"
+    },
+    {
+        titulo: "A Hora da Estrela",
+        resumo: "Um dos romances mais importantes da literatura brasileira contemporânea, que narra a história de Macabéa, uma jovem solitária e marginalizada",
+        imagem: "https://s5.static.brasilescola.uol.com.br/be/2022/08/capa-livro-hora-da-estrela.jpg",
+        tags: "hora estrela Hora Estrela"
+    },
+    {
+        titulo: "O Beijo da Mulher Aranha",
+        resumo: "Um romance que se passa em uma prisão, explorando temas como a homossexualidade, a amizade e a política",
+        imagem: "https://upload.wikimedia.org/wikipedia/pt/8/8b/Kiss_Of_The_Spiderwoman.jpg",
+        tags: "beijo mulher aranha Beijo Mulher Aranha"
+    },
+    {
+        titulo: "A Cidade e as Serras",
+        resumo: "Uma sátira social que compara a vida na cidade com a vida no campo, revelando os valores da burguesia portuguesa",
+        imagem: "https://guiadoestudante.abril.com.br/wp-content/uploads/sites/4/2016/10/cidades-serra-capa2.jpg?quality=70&strip=info&w=360",
+        tags: "Cidade Serras cidade serras"
+    }
+]
